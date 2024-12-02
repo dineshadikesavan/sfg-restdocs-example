6c0936104b3ed6a26c44afb2fc85c65e7e5d9688
Dinesh A <dinesh.adikesavan@gmail.com>
2024-12-01 20:43:46 -0600

    initial commit

---
+++ src/test/java/com/tsc/sfgrestdocsexample/SfgRestdocsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsc.sfgrestdocsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRestdocsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
